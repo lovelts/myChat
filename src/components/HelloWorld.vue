08a8737488cd01c8a4a95c0ff13ad9e24a2e2a35
lovelts <lts.lyc@qq.cpm>
2020-11-04 09:21:31 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+a {
+  color: #42b983;
+}
+</style>
